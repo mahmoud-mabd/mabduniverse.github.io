a700ed133e3533c75e4b888d876408d4c86f0cf6
MABD <info@mabduniverse.com>
2023-11-19 22:15:17 +0200

    Removed testing code

--- script.js
+++ script.js
@@ -4,17 +4,12 @@ import { createConnectedParticlesEffect } from "./effects/ConnectedParticlesEffe
 
 
 const canvas = document.getElementById('canvas');
-const canvas2 = document.getElementById('canvas2');
 
-canvas.width = 200 //window.innerWidth
-canvas.height = 200 //window.innerHeight
-
-canvas2.width = 200
-canvas2.height = 200
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 const effects = []
-effects.push(createConnectedParticlesEffect(canvas, undefined, 15))
-effects.push(createConnectedParticlesEffect(canvas2, undefined, 15))
+effects.push(createConnectedParticlesEffect(canvas, undefined, 150))
 
 function animate() {
     effects.forEach(effect => {
